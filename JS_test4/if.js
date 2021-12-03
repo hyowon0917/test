34ae8b982ed17df2e826dab8635d45e853b6a32f
HyowonKim <hyonii917@gmail.com>
2021-12-03 20:43:10 +0900

    Add files via upload

---
+++ JS_test4/if.js
@@ -0,0 +1,10 @@
+let isShow = true;
+let checked = false;
+
+if (isShow) {
+  console.log('HYOWON');  // HYOWON
+}
+
+if (checked) {
+  console.log('happy');
+}
---
+++ JS_test4/if_else.js
@@ -0,0 +1,7 @@
+let isShow = true;
+
+if(isShow) {
+  console.log('hyowon');
+} else {
+ console.log('not there');
+}
